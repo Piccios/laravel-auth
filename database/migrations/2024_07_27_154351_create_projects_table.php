8ea7c4ccdb3335663bfc951e3df8fd2cd1edfbd1
Piccio <lorenzo.hl.picchi@gmail.com>
2024-07-27 18:41:57 +0200

    created a seeder and a migration for GitHub projects

--- database/migrations/2024_07_27_154351_create_projects_table.php
+++ database/migrations/2024_07_27_154351_create_projects_table.php
@@ -1,10 +1,11 @@
 <?php
 
+use App\Models\Project;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
 return new class extends Migration
+
 {
     /**
      * Run the migrations.
@@ -13,7 +14,11 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('nome', 255);
+            $table->string('linguaggio', 255);
+            $table->string('url_repository')->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
