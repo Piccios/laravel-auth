20d85c21aa6238df943274cae6f702f6af74c930
Piccio <lorenzo.hl.picchi@gmail.com>
2024-07-27 19:57:03 +0200

    header created

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -23,17 +23,13 @@
                     <a class="nav-link" aria-current="page" href="{{ route('admin.project.create')}}">Crea nuovo progetto</a>
                 </li>
             </ul>
-
-            <!-- Right Side Of Navbar -->
             <ul class="navbar-nav ms-auto">
-                <!-- Authentication Links -->
                 @guest
                     @if (Route::has('login'))
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                         </li>
                     @endif
-
                     @if (Route::has('register'))
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
@@ -44,7 +40,6 @@
                         <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                             {{ Auth::user()->name }}
                         </a>
-
                         <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                             <a class="dropdown-item" href="/home">
                                 Homepage
@@ -54,7 +49,6 @@
                                             document.getElementById('logout-form').submit();">
                                 {{ __('Logout') }}
                             </a>
-
                             <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                 @csrf
                             </form>
