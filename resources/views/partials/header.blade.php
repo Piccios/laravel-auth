b050ec31d4ceaedff622d9bf5d5e9c9f4d0e6056
Piccio <lorenzo.hl.picchi@gmail.com>
2024-07-28 17:21:29 +0200

    app name correction test

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,24 +3,26 @@
         <a class="navbar-brand" href="{{ url('/') }}">
             {{ config('app.name', 'Piccio\'s portfolio') }}
         </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-icon"></span>
         </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <!-- Left Side Of Navbar -->
             <ul class="navbar-nav me-auto">
                 <li>
                     <a class="nav-link" href="/home">
-                    Homepage</a>
+                        Homepage</a>
                 </li>
                 <li>
-                    <a class="nav-link" href="{{ route('admin.projects.index')}}"  aria-current="page">
+                    <a class="nav-link" href="{{ route('admin.projects.index') }}" aria-current="page">
                         Progetti
                     </a>
                 </li>
                 <li>
-                    <a class="nav-link" aria-current="page" href="{{ route('admin.projects.create')}}">Crea nuovo progetto</a>
+                    <a class="nav-link" aria-current="page" href="{{ route('admin.projects.create') }}">
+                        Crea nuovo progetto
+                    </a>
                 </li>
             </ul>
             <ul class="navbar-nav ms-auto">
@@ -37,7 +39,8 @@
                     @endif
                 @else
                     <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                            data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                             {{ Auth::user()->name }}
                         </a>
                         <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
